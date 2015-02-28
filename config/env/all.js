52f8547fd3b0c1a377533e7135c3ff77bb7e2828
jackypan1989 <jackypan1989@gmail.com>
2015-02-28 01:58:58 -0800

    update

---
+++ config/env/all.js
@@ -0,0 +1,52 @@
+'use strict';
+
+module.exports = {
+	app: {
+		title: 'Breakdown',
+		description: 'Full-Stack JavaScript with MongoDB, Express, AngularJS, and Node.js',
+		keywords: 'MongoDB, Express, AngularJS, Node.js'
+	},
+	port: process.env.PORT || 3000,
+	templateEngine: 'swig',
+	sessionSecret: 'MEAN',
+	sessionCollection: 'sessions',
+	assets: {
+		lib: {
+			css: [
+				'public/lib/bootstrap/dist/css/bootstrap.css',
+				'public/lib/bootstrap/dist/css/bootstrap-theme.css',
+			],
+			js: [
+				'public/lib/jquery/jquery.js',
+				'public/lib/angular/angular.js',
+				'public/lib/angular-resource/angular-resource.js', 
+				'public/lib/angular-cookies/angular-cookies.js', 
+				'public/lib/angular-animate/angular-animate.js', 
+				'public/lib/angular-touch/angular-touch.js', 
+				'public/lib/angular-sanitize/angular-sanitize.js', 
+				'public/lib/angular-ui-router/release/angular-ui-router.js',
+				'public/lib/angular-ui-utils/ui-utils.js',
+				'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
+				'public/lib/flowchart/svg_class.js',
+				'public/lib/flowchart/mouse_capture_service.js',
+				'public/lib/flowchart/dragging_service.js',
+				'public/lib/flowchart/flowchart_viewmodel.js',
+				'public/lib/flowchart/flowchart_directive.js',
+				'public/lib/lodash/lodash.js'
+			]
+		},
+		css: [
+			'public/modules/**/css/*.css'
+		],
+		js: [
+			'public/config.js',
+			'public/application.js',
+			'public/modules/*/*.js',
+			'public/modules/*/*[!tests]*/*.js'
+		],
+		tests: [
+			'public/lib/angular-mocks/angular-mocks.js',
+			'public/modules/*/tests/*.js'
+		]
+	}
+};
