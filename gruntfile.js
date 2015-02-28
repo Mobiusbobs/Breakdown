1ab0ceae4082c9f5c9b17c93fa0a04ab8cf889f9
jackypan1989 <jackypan1989@gmail.com>
2015-02-28 12:31:45 -0800

    update bower

--- gruntfile.js
+++ gruntfile.js
@@ -158,7 +158,7 @@ module.exports = function(grunt) {
 	});
 
 	// Default task(s).
-	grunt.registerTask('default', ['concurrent:default']);
+	grunt.registerTask('default', ['lint', 'concurrent:default']);
 
 	// Debug task.
 	grunt.registerTask('debug', ['lint', 'concurrent:debug']);
