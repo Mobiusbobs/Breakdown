442ce66e60cbac921674f6a365f756edda71d531
Ray Shih <mnf.shih@gmail.com>
2015-02-28 18:47:39 -0800

    update gruntfile setting for react

---
+++ public/modules/core/react/componenets/InterfaceEditor.jsx
@@ -0,0 +1,18 @@
+/** @jsx React.DOM */
+var InterfaceEditor = React.createClass({
+  render: function() {
+    var style = {
+      height: "100%"
+    };
+
+		return (
+      <div className="col-md-3" style={style}>
+        <div className="panel panel-default">
+          <div className="panel-body">
+            Input
+          </div>
+        </div>
+      </div>
+    );
+  }
+});
